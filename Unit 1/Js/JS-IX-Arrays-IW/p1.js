f6e9e7a5d8bbc72a98e6dc227dc4da38a12b1a40
Arsalan Shaikh <arsalan4coder@gmail.com>
2022-04-25 21:50:01 +0530

    arrays

---
+++ Unit 1/Js/JS-IX-Arrays-IW/p1.js
@@ -0,0 +1,9 @@
+// Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+
+
+
+let arr=["bat","ball", "mobile"]
+for (i=0;i<arr.length;i++){
+  console.log(i+1,arr[i])
+}
---
+++ Unit 1/Js/JS-IX-Arrays-IW/p2.js
@@ -0,0 +1,22 @@
+// Problem 2 : Given a character in lower case print the upper case character
+
+
+
+
+
+
+let char ="g"
+
+let upper =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+let lower =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+for (let i=0;i<lower.length;i++){
+   // console.log(lower[i])
+   // console.log(char)
+  if (char==lower[i]){
+     console.log(upper[i])
+   
+  }
+  
+ 
+}
